8039e1f22d522cf64d6ce1b7c771051d0ab4b5bd
”Camopogok” <zagrebin.dm@gmail.com>
2022-12-27 18:59:44 +0300

    Коммит 2.10.6

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Calc calc = new Calc();
+
         FileReader fr = new FileReader("input.txt");
         Scanner input = new Scanner(fr);
-        String s = input.nextLine();
-        fr.close();
-
         FileWriter fw = new FileWriter("Output.txt");
 
-        System.out.println(s);
-        try {
-            System.out.println(calc.printResult(s));
-            fw.write("" + calc.printResult(s));
-            fw.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        while (input.hasNextLine()) {
+            String s = input.nextLine();
+            Calc calc = new Calc();
+            try {
+                fw.write(s + " = " + calc.printResult(s) + '\n');
+            } catch (Exception e) {
+                fw.write(s + " = " + e.getMessage() + '\n');
+            }
         }
+        fr.close();
+        fw.close();
     }
 }
 
@@ -28,17 +28,15 @@ class Calc {
     double b = 0;
     String action;
     boolean error = false;
+    String resultText = "";
 
-    private void inputData (String symbols) throws Exception {
-        String [] data = symbols.split(" ");
+    private void inputData(String symbols) throws Exception {
+        String[] data = symbols.split(" ");
         try {
             a = Double.parseDouble(data[0]);
             b = Double.parseDouble(data[2]);
-        }
-        catch (NumberFormatException e) {
-            System.out.println("Error! Not number");
-            error = true;
-            return;
+        } catch (NumberFormatException e) {
+            throw new Exception("Error! Not number");
         }
         if ((data[1].equals("+") || data[1].equals("/") || data[1].equals("-") || data[1].equals("*"))) {
             action = data[1];
@@ -47,33 +45,32 @@ class Calc {
         }
     }
 
-    Double printResult (String s) throws Exception {
+    String printResult(String s) {
         try {
             inputData(s);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            resultText = e.getMessage();
             error = true;
         }
-        Double result = null;
         if (!error) {
             switch (action) {
                 case "+":
-                    result = a + b;
+                    resultText = "" + (a + b);
                     break;
                 case "-":
-                    result = a - b;
+                    resultText = "" + (a - b);
                     break;
                 case "*":
-                    result = a * b;
+                    resultText = "" + (a * b);
                     break;
                 case "/":
                     if (b != 0)
-                        result = a / b;
+                        resultText = "" + (a / b);
                     else
-                        throw new Exception("Error! Division by zero");
+                        resultText = "Error! Division by zero";
                     break;
             }
         }
-        return result;
+        return resultText;
     }
 }
